1f554406003ee25540fd813f143bdb796f38bd8e
Nadia Naely <nadianaely32@gmail.com>
2023-06-20 17:16:29 -0300

    Criação classe personagem

---
+++ Projeto RPG/Personagem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_RPG
+{
+    internal class Personagem
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
