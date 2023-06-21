fe0504766c9a48234b4ba9a9963191a7cd4cbb0b
Labes <gdlabes8@gmail.com>
2023-06-21 13:52:23 -0300

    Adição de pasta e personagem na pasta

---
+++ Projeto RPG/Personagem/Personagem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_RPG
+{
+    internal class Personagem
+    {
+        public string Nome { get; set ; }
+        public int PontosVida { get; set ; }
+        public int Forca { get; set ; }
+        public int Defesa { get; set ; }
+
+        public Personagem() { }
+
+        public void Atacar() {  }
+        public void Fugir() { }
+        public void Defender() { }
+        public void UsarHabilidade() { }
+
+
+
+    }
+}
