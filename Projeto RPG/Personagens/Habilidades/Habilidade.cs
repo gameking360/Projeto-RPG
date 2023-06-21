e00b110bbeaa42932168486a76fb63c14dab7fa6
Bernardo <bernardo.ch.be@gmail.com>
2023-06-21 16:17:06 -0300

    Adicionando habilidades

---
+++ Projeto RPG/Personagens/Habilidades/Habilidade.cs
@@ -0,0 +1,28 @@
+﻿using Projeto_RPG.Personagens.Habilidades.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_RPG.Personagens.Habilidades
+{
+    internal class Habilidade
+    {
+        public string Nome { get; set; }
+        public Tipos Tipo { get; set; }
+        public Tipos Fraqueza { get; set; }
+        public Efeito Efeito { get; set; }
+
+        public int Dano { get; set; }
+
+        public Habilidade(string nome, Tipos tipos, Tipos fraqueza, Efeito efeito, int dano)
+        {
+            Nome = nome;
+            Tipo = tipos;
+            Fraqueza = fraqueza;
+            Efeito = efeito;
+            Dano = dano;
+        }
+    }
+}
---
+++ Projeto RPG/Personagens/Habilidades/Magia.cs
@@ -0,0 +1,19 @@
+﻿using Projeto_RPG.Personagens.Habilidades.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_RPG.Personagens.Habilidades
+{
+    internal class Magia : Habilidade
+    {
+        public int Custo { get; set; }
+
+        public Magia(string nome, Tipos tipo, Tipos fraqueza, Efeito efeito, int dano, int custo) : base(nome, tipo, fraqueza, efeito, dano)
+        {
+            Custo = custo;
+        }
+    }
+}
