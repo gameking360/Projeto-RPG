48fbbdcc915cbdf1df1ccbdef703158fdaf2e0fc
Vinicius <viniciusgrossert@gmail.com>
2023-06-21 15:48:07 -0300

    Atributos Inimigos

---
+++ Projeto RPG/Personagens/Inimigo/Inimigo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_RPG.Personagens.Inimigo
+{
+    internal class Inimigo : Personagem
+    {
+        public override string Nome { get; set; }
+        public override int PontosVida { get; set; }
+        public override int Forca { get; set; }
+        public override int Defesa { get; set; }
+        public  string Tipo { get; set; }
+        public int RecompensaXP { get; set; }
+
+        public Inimigo(string nome, int pontosvida, int forca, int defesa, string tipo, int recompensaXP)
+        {
+            Nome = nome;
+            PontosVida = pontosvida;
+            Forca = forca;
+            Defesa = defesa;
+            Tipo = tipo;
+            RecompensaXP = recompensaXP;
+        }
+
+        public override void Atacar() { }
+
+        public override void UsarHabilidade() { }
+
+        public override void Defender() { }
+
+        public override void Fugir() { }
+    }
+}
